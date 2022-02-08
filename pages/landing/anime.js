adddb40c7d5615e323c33cff3b49aaa8dddec8ff
Yajush Vyas <dryashhoney@gmail.com>
2022-02-08 18:54:50 +0530

    added new way to implement css

--- pages/landing/anime.js
+++ pages/landing/anime.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 const Anime = () => (
-    <div>
-        <h1 className="font-Fjalla text-base">Almost before we knew it, we had left the ground.</h1>
+    <div className="w-screen h-screen relative bg-black">
+      <div class=" mx-144 md:sm-2 ">
+        <div class=" py-5">    
+          <nav class="hidden md:flex space-x-20">
+            <a href="#" class="text-xl font-Fjalla text-white hover:text-gray-500"> HOME </a>
+            <a href="#" class="text-xl font-Fjalla text-white hover:text-gray-500"> ABOUT </a>
+            <a href="#" class="text-xl font-Fjalla text-white hover:text-gray-500"> HELP </a>
+            <a href="#" class="text-xl font-Fjalla text-white hover:text-gray-500"> CLASS </a>
+          </nav>
+        </div>
+      </div>
     </div>
   );
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    
     extend: {
       fontFamily: {
         Fjalla: ["Fjalla One", "sans-serif"],
-     },
+        },
+        spacing: {
+          '13': '3.25rem',
+          '15': '3.75rem',
+          '128': '32rem',
+          '144': '36rem',
+        },
     },
   },
   plugins: [],
