4dad6c455a84475fe93b571903d821f5c6b66777
Yajush Vyas <dryashhoney@gmail.com>
2022-02-08 19:07:08 +0530

    Added components

---
+++ pages/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => (
+    <div className="w-screen h-screen relative bg-black">
+      <div className=" mx-144 md:sm-2 ">
+        <div className=" py-5">    
+          <nav className="hidden md:flex space-x-20">
+            <a href="#" className="text-xl font-Fjalla text-white hover:text-gray-500"> HOME </a>
+            <a href="#" className="text-xl font-Fjalla text-white hover:text-gray-500"> ABOUT </a>
+            <a href="#" className="text-xl font-Fjalla text-white hover:text-gray-500"> HELP </a>
+            <a href="#" className="text-xl font-Fjalla text-white hover:text-gray-500"> CLASS </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+  
+  export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Anime from "./landing/anime"
+import Header from "./Components/Header/Header"
 
 export default function Home() {
   return (
@@ -8,7 +8,7 @@ export default function Home() {
         <title>AnbuWeebs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Anime/>
+      <Header/>
     </div>
   );
 }
