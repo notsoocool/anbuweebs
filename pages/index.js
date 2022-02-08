38864df57271f8cc825df53093a9c9546e461ccd
Yajush Vyas <dryashhoney@gmail.com>
2022-02-08 19:22:52 +0530

    header push

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "./Components/Header/Header"
+import Hero from './Components/Hero/Hero'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
+      <Hero/>
     </div>
   );
 }
